86470876e70c5ec54476150ee9b0895bb323a058
lupalhan <lupalhan@student.42.rio>
2025-10-24 19:05:03 -0300

    Add isprint function

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lupalhan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lupalhan <lupalhan@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/24 15:21:27 by lupalhan          #+#    #+#             */
-/*   Updated: 2025/10/24 18:30:16 by lupalhan         ###   ########.fr       */
+/*   Created: 2025/10/24 18:27:39 by lupalhan          #+#    #+#             */
+/*   Updated: 2025/10/24 18:38:46 by lupalhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-#endif
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	return (0);
+}
