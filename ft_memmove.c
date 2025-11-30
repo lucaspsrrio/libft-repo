ddbc3b9a087fd1b40bd73fd31c1c324df0f11ea6
Lucas Palhano <lucaspsrrio@gmail.com>
2025-11-30 17:07:07 -0300

    Add ft_memmove function

---
+++ ft_memmove.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lupalhan <lupalhan@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 15:37:15 by lupalhan          #+#    #+#             */
+/*   Updated: 2025/11/30 17:02:44 by lupalhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	if (!dest && !src)
+		return (NULL);
+	if (d > s)
+	{
+		while (n-- > 0)
+			d[n] = s[n];
+	}
+	else
+		ft_memcpy(dest, src, n);
+	return (dest);
+}
